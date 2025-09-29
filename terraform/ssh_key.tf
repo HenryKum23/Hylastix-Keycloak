c9e00d9b1855571e180b83a2bb3f48ca9f31ad9c
HenryKum23 <slypee1881@gmail.com>
2025-09-30 00:05:22 +0200

    updated terraform files

--- terraform/ssh_key.tf
+++ terraform/ssh_key.tf
@@ -3,13 +3,6 @@ resource "tls_private_key" "ssh_key" {
   rsa_bits  = 4096
 }
 
-#Save the public key to a file
-# resource "local_file" "public_key_file" {
-#   content  = tls_private_key.ssh_key.public_key_openssh
-#   filename = "${path.module}/id_rsa.pub"
-#   file_permission = "0644"
-# }
-
 #Save the private key to a file with restricted permissions
 resource "local_file" "private_key_file" {
   content         = tls_private_key.ssh_key.private_key_pem
